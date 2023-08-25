2bb1cc0bf8a1f7f54010639940eee6e14875818b
Kumar Harsh <kharsh560@gmail.com>
2023-08-25 20:11:47 +0530

    First stage completed
    Very first stage of my project on making a music streaming website is completed. I have done all the HTML and CSS work needed for desktop version of this site!

--- dhunFunctioning.js
+++ dhunFunctioning.js
@@ -1,1 +1,4 @@
-
+// document.querySelector(".playerButtons").addEventListener("click", function (clickEvent) {
+//     document.querySelector(".playerButtons").style.height = "20px";
+//     document.querySelector(".playerButtons").style.width = "20px";
+// })
